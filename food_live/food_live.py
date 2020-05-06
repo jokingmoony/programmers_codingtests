803005fd9290e48707fcd4ddf28a7307d5a4f7d8
yongkim <penubokim@gmail.com>
2020-05-06 11:08:41 +0900

    food live in python accuracy 10%, speed 8%

--- food_live/food_live.py
+++ food_live/food_live.py
@@ -1,27 +1,44 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42891
-
 from collections import deque
 
 
 def solution(food_times, k):
-    answer = 0
-    index = 0
+    
     n = len(food_times)
     foods = deque()
     food_number = 0
     
+    times = int(k / n)
+    offset = k % n
+    
     for index, food in enumerate(food_times):
-        foods.append((index + 1, food))
+        foods.append([index + 1, food])
         
-    while k > 0:
+    while True:
+        foods = deque(map(lambda x : [x[0], x[1] - times], foods))
+
+        rest = -1 * sum(map(lambda x : x[1], list(filter(lambda x : x[1] < 0, foods))))
+        foods = deque(filter(lambda x : x[1] > 0, foods))
+
+        n = len(foods)
+        
+        if n == 0:
+            return -1
+        if rest == 0:
+            break
+        
+        times = int(rest / n)
+        
+    while offset > 0:
         if len(foods) == 0:
             return -1
         food_number, food_time = foods.popleft()
         food_time -= 1
         if food_time != 0:
             foods.append((food_number, food_time))
-        k -= 1
+        offset -= 1
 
     if len(foods) == 0:
         return -1
     return foods[0][0]
+    
