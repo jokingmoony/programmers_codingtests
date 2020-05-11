34db4b95dbc78462767d260ff6ab33af6fe485d9
yongkim <penubokim@gmail.com>
2020-05-11 23:39:49 +0900

    chusuck traffic done in python

--- chusuck_traffic/chusuck_traffic.py
+++ chusuck_traffic/chusuck_traffic.py
@@ -8,20 +8,18 @@ def solution(lines):
     for line in lines:
         _, end_time, time = line.split()
         end = datetime.datetime.strptime(end_time,'%H:%M:%S.%f')
-        time = datetime.timedelta(seconds=float(time[:-1]))
+        time = float(time[:-1]) - 0.001
+        time = datetime.timedelta(seconds=time)
         start = end - time
         traffics.append((start, end))
-    
+
     for start, end in traffics:
-        td = datetime.timedelta(seconds=1)
+        td = datetime.timedelta(seconds=0.999)
         times = [(start, start + td), (start - td, start), (end, end + td), (end - td, end)]
-        pprint(times)
-        # get time
         for s, e in times:
             tmp = 0
-
             for start, end in traffics:
-                if (start >= s or end <= e) or (start <= s and end >= e):
+                if (start >= s and start <= e) or (end >= s and end <= e) or (start <= s and end >= e):
                     tmp += 1
             if tmp > answer:
                 answer = tmp
