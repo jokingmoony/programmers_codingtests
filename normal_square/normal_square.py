c33d24e895faeaf7c5aa7fee5db98adce7dd92f4
yongkim <penubokim@gmail.com>
2020-05-21 17:02:25 +0900

    normal square fail in python improve speed

--- normal_square/normal_square.py
+++ normal_square/normal_square.py
@@ -1,13 +1,13 @@
 # https://programmers.co.kr/learn/courses/30/lessons/62048
+import math
 
 def check(arr):
     if min(arr) * max(arr) < 0:
         return True
     return False
 
-def solution(w,h):
-    answer = w * h
-    
+def getEmpty(w, h):
+    cnt = 0
     for r in range(w):
         for c in range(h):
             tmp = []
@@ -15,7 +15,17 @@ def solution(w,h):
                 tmp.append(h/w * x - y)
             
             if check(tmp):
-                answer -= 1
+                cnt += 1
+
+    return cnt
+
+def solution(w,h):
+    answer = w * h
+    ww = w
+    hh = h
+    g = math.gcd(w, h)
+    
+    answer -= (getEmpty(w//g, h//g) * g)
             
                 
     return answer
