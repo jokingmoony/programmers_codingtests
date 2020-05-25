2df4268ce75b4595c80c66cb4ddd265d7c368666
yongkim <penubokim@gmail.com>
2020-05-25 14:19:00 +0900

    ground edit done in python

--- ground_edit/ground_edit.py
+++ ground_edit/ground_edit.py
@@ -1,31 +1,25 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12984
 
-def process(land, mid, P, Q):
-    cost = 0
-    for rows in land:
-        for block in rows:
-            if block < mid:
-                cost += (mid - block) * P
-            else:
-                cost += (block - mid) * Q
-
-    return cost
 
 def solution(land, P, Q):
+
+    land_1d = []
+    total = 0
+    for i in land:
+        for j in i:
+            land_1d.append(j)
+            total += j
+
+    land_1d.sort()
+    prev = 0
     answer = float('inf')
-    
-    start = 1
-    end = 10000000000
-    
-    while start < end:
-        
-        mid = (start + end) // 2
-        cost = process(land, mid, P, Q)
-        if answer > cost:
-            answer = cost
-            end = mid - 1
-        else:
-            start = mid + 1
-            
+    already = -1
+    for i, l in enumerate(land_1d):
+        if l != already:
+            up = l * i - prev
+            down = total - prev - (len(land_1d) - i) * l
+            answer = min(answer, P*up + Q*down)
+            already = l
+        prev += l
     
     return answer
