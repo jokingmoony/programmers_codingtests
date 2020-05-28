d1deb16e3b89b62257569fd88130df790aca180f
yongkim <penubokim@gmail.com>
2020-05-28 13:02:48 +0900

    algebra problem done in python using matrix chain multiplication algorithm dp

--- algebra_problem/algebra_problem.py
+++ algebra_problem/algebra_problem.py
@@ -1,33 +1,28 @@
 # https://programmers.co.kr/learn/courses/30/lessons/1843
-def get_max(arr):
-    res = 0
-    if not '-' in arr:
-        return sum(map(lambda x: int(x), arr[0::2]))
-    
-    for i in range(len(arr)):
-        if arr[i] == '-':
-            res = max(res, get_max(arr[:i]) - get_min(arr[i+1:]))
-    return res
-    
-def get_min(arr):
-    res = float('inf')
-    if not '-' in arr:
-        return sum(map(lambda x: int(x), arr[0::2]))
-    
-    for i in range(len(arr)):
-        if arr[i] == '-':
-            res = min(res, get_min(arr[:i]) - get_max(arr[i+1:]))
-    
-    return res
-
-
 def solution(arr):
-    answer = 1
-    for i in range(len(arr)):
-        cost = 0
-        if arr[i] == '-':
-            cost = get_max(arr[:i]) - get_min(arr[i+1:])
-            if cost > answer:
-                answer = cost
-            
-    return answer
+    answer = 0
+    n = len(arr) // 2 + 1
+    nums = arr[::2]
+    
+    dp = [[[0, 0] for i in range(n+1)] for i in range(n+1)]
+    for i in range(1, n+1):
+        for j in range(2):
+            dp[i][i][j] = int(nums[i-1])
+
+    for L in range(2, n+1):
+        for i in range(1, n-L+2):
+            j = i + L - 1
+            dp[i][j][0] = -float('inf')
+            dp[i][j][1] = float('inf')
+            for k in range(i, j-1+1):
+                if arr[2 * k - 1] == '-':
+                    cost_max = dp[i][k][0] - dp[k+1][j][1]
+                    cost_min = dp[i][k][1] - dp[k+1][j][0]
+                else:
+                    cost_max = dp[i][k][0] + dp[k+1][j][0]
+                    cost_min = dp[i][k][1] + dp[k+1][j][1]
+                
+                dp[i][j][0] = max(cost_max, dp[i][j][0])
+                dp[i][j][1] = min(cost_min, dp[i][j][1])
+    
+    return dp[1][n][0]
