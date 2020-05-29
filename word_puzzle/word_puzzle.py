9ae636757498e472634984b600f76ffa4d0bcead
yongkim <penubokim@gmail.com>
2020-05-29 13:41:20 +0900

    update word puzzle failed in python timeout

--- word_puzzle/word_puzzle.py
+++ word_puzzle/word_puzzle.py
@@ -1,23 +1,20 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12983
 
 def solution(strs, t):
-    answer = 0
-    
     n = len(t)
-    dp = [[float('inf') for _ in range(n+1)] for _ in range(n+1)]
-    for word in strs:
-        for i, c in enumerate(t):
-            if t[i:].startswith(word):
-                dp[i+1][i+len(word)] = 1
-                
-    for L in range(2, n+1):
-        for i in range(1, n-L+2):
-            j = i + L - 1
-#             dp[i][j]
-            for k in range(i, j-1+1):
-                dp[i][j] = min(dp[i][k] + dp[k+1][j], dp[i][j])
+    dp = [float('inf') for _ in range(n)]
+    for i in range(1, n+1):
+        for word in strs:
+            l = len(word)
+            if l > i:
+                continue
+            if word == t[i-l:i]:
+                if i - l == 0:
+                    dp[i-1] = 1
+                    continue
+                dp[i-1] = min(dp[i-1], dp[i-l-1]+1)
     
-    if dp[1][n] == float('inf'):
+    answer = dp[n-1]
+    if answer == float('inf'):
         return -1
-    else:
-        return dp[1][n]
+    return answer
